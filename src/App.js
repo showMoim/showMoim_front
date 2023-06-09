dec6b3320946e49cf2922afb80ec1986b5792750
dlwldk <dlwldk1030@naver.com>
2023-06-09 14:00:48 +0900

    merge test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Login/Login';
 import SignUp from './Login/SignUp';
 import Main from './Main/Main';
@@ -7,13 +7,14 @@ import './App.css';
 
 function App() {
   const isLogin = false;
+
   return (
     <Router>
     <Routes>
       <Route path="/Login" element={<Login />} />
       <Route path="/SignUp" element={<SignUp />} />
       <Route path="/Main" element={<Main />} />
-      <Route path="/" render={() => (isLogin ? <Redirect to="/Main" /> : <Login />)} />
+      <Route path="/" render={() => (isLogin ? <Navigate to="/Main" /> : <Navigate to="/Login" />)} />
       {/* <Route
           path="/login"
           render={() => (isLogin ? <Redirect to="/" /> : <Login />)}
